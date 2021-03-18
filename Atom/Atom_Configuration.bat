eb095a79a5188c0aab4fdacabe7cbe920803ec57
Jesse James Isler <jesse.isler@gmail.com>
2021-03-18 14:09:44 +0100

    Updated ~~localisation~~ configuration files...

--- Atom/Atom_Configuration.bat
+++ Atom/Atom_Configuration.bat
@@ -2,11 +2,12 @@
 echo Is atom installed on your system? If no, please install it first before running this program!
 pause
 
-apm install atom-material-ui language-batchfile language-powershell title-bar-replacer linter linter-ui-default intentions busy-signal linter-markdown teletype atom-clock
+apm install atom-material-ui language-batchfile language-powershell title-bar-replacer linter linter-ui-default intentions busy-signal teletype atom-clock
 
-del %USERPROFILE%/.atom/config.cson
-copy ./config.cson %USERPROFILE%/.atom/config.cson
+rem Whoops...
+rem del %USERPROFILE%/.atom/config.cson
+rem copy ./config.cson %USERPROFILE%/.atom/config.cson
 
 echo Finished installing Atom-James edition. Press any key to close!
 echo You probably want to set-up Teletype and Github now...
-pause
+pause > nul
